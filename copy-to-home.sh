ec6dde64d9b417ed6053fdccbe5204ce810e10b4
Amaresh R. Joshi <joshia@msu.edu>
2017-08-21 12:02:54 -0400

    Added a delete olf files and copy new script.

---
+++ copy-to-home.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#
+# copy config files into home directory, deleting
+# any old files and folders first
+
+rm -rvf ${HOME}/.bash.d
+rm -vf ${HOME}/.bash_logout
+rm -vf ${HOME}/.bash_profile
+rm -vf ${HOME}/.bashrc
+rm -vf ${HOME}/.tmux.conf
+
+
+
+EH="${HOME}/.emacs.d"
+
+rm -rvf ${EH}/lisp
+rm -rvf ${EH}/lisp-old
+rm -rvf ${EH}/themes
+
+#
+# now copy files to home
+
+cp -rv .[bet]* ${HOME}/
+
+
+
+
