41693a11536397a5b6acc9a3ae6fe469fb1b5b4a
Amaresh R. Joshi <joshia@msu.edu>
2017-10-30 09:47:55 -0400

    Added window sizing and positioning to emacs init.el.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -149,6 +149,33 @@
        (menu-bar-mode -1)
        )
 )
+;;
+;; set size dynamically
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 160 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 160))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+;;
+;; set position (this needs some tweaking)
+(if window-system
+    (progn
+      (add-to-list 'default-frame-alist (cons 'top 50))
+      (add-to-list 'default-frame-alist (cons 'left 50))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
