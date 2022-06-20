77009404214339a0b00daef8b2816e046a288282
Amaresh R Joshi <joshia@msu.edu>
2022-06-20 08:41:12 -0400

    Added elisp function to ssh/ramp to humcore on the production box.

--- .emacs.d/lisp/msucommons.el
+++ .emacs.d/lisp/msucommons.el
@@ -1,7 +1,10 @@
 ;;
 ;; humcore utility functions
 
-(defun visit-humcore ()
+;;
+;; should really combine these into a single "general" command
+
+(defun visit-humcore-dev ()
   "Go to the humcore folder on the Commons development server."
   (interactive)
   (progn
@@ -10,4 +13,16 @@
     (setenv "SHELL" "/bin/bash")
     (setenv "ESHELL" "/bin/bash")
     (find-file "/ssh:ubuntu@msudev:/srv/www/commons/current/web/app/plugins/humcore/.")))
+
+(defalias 'visit-humcore 'visit-humcore-dev)
+
+(defun visit-humcore-prod ()
+  "Go to the humcore folder on the Commons development server."
+  (interactive)
+  (progn
+    ;;
+    ;; to open up bash instead of zsh on the remote server
+    (setenv "SHELL" "/bin/bash")
+    (setenv "ESHELL" "/bin/bash")
+    (find-file "/ssh:ubuntu@hcommons:/srv/www/commons/current/web/app/plugins/humcore/.")))
 
